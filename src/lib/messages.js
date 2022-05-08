e4e78a38f42ac12bb592276341e2e8f202d346aa
bakedzeedee <nacelle@protonmail.com>
2022-05-08 13:19:40 -0400

    feature: added commands and messages

---
+++ src/lib/messages.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        "message": "Type !cmd for a list of commands",
+        "interval": 5000000,
+        "delay": 30000
+    },
+    {
+        "message": "Type !sfx for a list of sounds",
+        "interval": 5000000,
+        "delay": 90000
+    }
+]
